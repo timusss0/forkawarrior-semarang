f5ebd1a660138286cc7184b23be9b12f3e5febfc
timusss0 <tiamustika73@gmail.com>
2023-12-03 19:07:57 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+tailwind.config = {
+  theme: {
+      fontFamily: {
+          'sans': ['Poppins', 'Arial', 'sans-serif'],
+          'serif': ['Playfair Display', 'Arial', 'sans-serif'],
+      }
+  },
+}
